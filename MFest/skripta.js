fb8fba5d52fe702addf5c5551768ea0bee69d057
Azra Mahmutovic <azramah6@gmail.com>
2015-04-15 21:07:13 +0200

    Meni
    dropdown meni na "about us"

---
+++ MFest/skripta.js
@@ -0,0 +1,12 @@
+
+function show(){
+	document.getElementById("dropdown").style.visibility = "visible";
+}
+
+function boja(item){
+	item.style.background = "rgba(0,0,0,0.4)";
+}
+
+function stara(item){
+	item.style.background = "rgba(0,0,0,0)";
+}
