de3030c1a6c3dc991d6f1686aa5f0e19f391ee3b
Azra Mahmutovic <azramah6@gmail.com>
2015-05-28 23:51:51 +0200

    Update

--- MFest/spasikomentar.php
+++
@@ -1,5 +0,0 @@
-<?php
-            $ime = htmlEntities($_POST['name'], ENT_QUOTES);
-            $mail = htmlEntities($_POST['email'], ENT_QUOTES);
-            $kom = htmlEntities($_POST['message'], ENT_QUOTES);
-?>
